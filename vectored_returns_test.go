342bba0563923affd8f3c6ad75fb63766b4f0a87
Yota Toyama <raviqqe@gmail.com>
2018-11-19 00:52:39 +0000

    Add vectored returns experiment

---
+++ vectored_returns_test.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/k0kubun/pp"
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+var caseFunctionType = llvm.PointerType(
+	llvm.FunctionType(
+		llvm.PointerType(llvm.Int8Type(), 0),
+		[]llvm.Type{llvm.PointerType(llvm.Int8Type(), 0)},
+		false,
+	),
+	0,
+)
+
+func TestVectoredReturnsWithPointersToResults(t *testing.T) {
+	m := llvm.NewModule("foo")
+
+	f := llvm.AddFunction(
+		m,
+		"constructor",
+		llvm.FunctionType(
+			llvm.PointerType(llvm.Int8Type(), 0),
+			[]llvm.Type{
+				llvm.PointerType(llvm.Int8Type(), 0),
+				llvm.ArrayType(caseFunctionType, 1),
+			},
+			false,
+		),
+	)
+	f.FirstParam().SetName("environment")
+	f.AddFunctionAttr(llvm.GlobalContext().CreateStringAttribute("noinline", ""))
+
+	b := llvm.NewBuilder()
+	b.SetInsertPointAtEnd(llvm.AddBasicBlock(f, ""))
+
+	b.CreateRet(
+		b.CreateCall(b.CreateExtractValue(f.Params()[1], 0, ""), []llvm.Value{f.FirstParam()}, ""),
+	)
+
+	llvm.VerifyFunction(f, llvm.AbortProcessAction)
+
+	f = llvm.AddFunction(
+		m,
+		"case",
+		llvm.FunctionType(
+			llvm.PointerType(llvm.Int8Type(), 0),
+			[]llvm.Type{llvm.PointerType(llvm.Int8Type(), 0)},
+			false,
+		),
+	)
+
+	b = llvm.NewBuilder()
+	b.SetInsertPointAtEnd(llvm.AddBasicBlock(f, ""))
+
+	b.CreateRet(f.FirstParam())
+
+	llvm.VerifyFunction(f, llvm.AbortProcessAction)
+
+	f = llvm.AddFunction(
+		m,
+		"switch",
+		llvm.FunctionType(
+			llvm.PointerType(llvm.Int8Type(), 0),
+			[]llvm.Type{llvm.PointerType(llvm.Int8Type(), 0)},
+			false,
+		),
+	)
+
+	b = llvm.NewBuilder()
+	b.SetInsertPointAtEnd(llvm.AddBasicBlock(f, ""))
+
+	b.CreateRet(
+		b.CreateCall(
+			m.NamedFunction("constructor"),
+			[]llvm.Value{
+				f.FirstParam(),
+				b.CreateInsertValue(
+					llvm.Undef(llvm.ArrayType(caseFunctionType, 1)),
+					m.NamedFunction("case"),
+					0,
+					"",
+				),
+			},
+			"",
+		),
+	)
+
+	llvm.VerifyFunction(f, llvm.AbortProcessAction)
+
+	m.Dump()
+
+	pb := llvm.NewPassManagerBuilder()
+	pb.SetOptLevel(3)
+
+	pm := llvm.NewFunctionPassManagerForModule(m)
+	pb.PopulateFunc(pm)
+
+	pm.RunFunc(f)
+
+	pm = llvm.NewPassManager()
+	pb.Populate(pm)
+	pp.Println(pm.Run(m))
+
+	m.Dump()
+}
